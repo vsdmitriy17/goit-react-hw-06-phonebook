2bb8ba0920e6e4c4f2fb4e8a2b148c39568e220a
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2022-07-01 01:22:41 +0300

    feature/phonebook_Redux_Toolkit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,32 @@
-import { useState, useEffect } from 'react';
 import styles from './App.module.css';
 import ContactList from './contactList/ContactList.jsx';
 import ContactForm from './contactForm/ContactForm.jsx';
 import Filter from './filter/Filter.jsx';
+import { addContact, deleteContact, filterContact } from "../redux/actions.js";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function App() {
-    const [contacts, setContacts] = useState(() => {
-        return JSON.parse(window.localStorage.getItem("contacts")) ?? [
-            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }
-        ];
-    });
-    const [filter, setFilter] = useState('');
-
-    useEffect(() => {
-        window.localStorage.setItem("contacts", JSON.stringify(contacts));
-    }, [contacts])
-
+    const contacts = useSelector(state => state.contactsReducer.items.filter(contact => contact.name.toLowerCase().includes(state.contactsReducer.filter)));
+    // console.log(contacts);
+    const filter = useSelector(state => state.contactsReducer.filter);
+    const dispatch = useDispatch();
     const formSubmit = data => {
-        setContacts(prevState => {
-            const isNameExist = prevState.find(contact => contact.name === data.name);
-            return (isNameExist ? alert(`${data.name} is already in contacts`) : [data, ...prevState]);
-        })
+        dispatch(addContact(data));
     };
-
-    const deleteContact = (currentId) => {
-        setContacts(prevState => {
-            return prevState.filter(contact => contact.id !== currentId)
-        });
-    };
-
-    const changeFilter = (evt) => {
-        setFilter(evt.currentTarget.value);
+    const changeFilter = data => {
+        dispatch(filterContact(data));
     }
-
-    const getVisibleContacts = () => {
-        const filterNormalized = filter.toLowerCase();
-        return contacts.filter(contact => contact.name.toLowerCase().includes(filterNormalized));
+    const deleteContacts = data => {
+        dispatch(deleteContact(data));
     }
-
-    const visibleContacts = getVisibleContacts();
-
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Phonebook</h1>
             <ContactForm onSubmit={formSubmit}/>
 
             <h2 className={styles.title}>Contacts</h2>
-            <Filter value={filter} onChangeFilter={changeFilter}/>
-            <ContactList contacts={visibleContacts} onDeleteContact={deleteContact}/>
+            <Filter filter={filter} onChangeFilter={changeFilter}/>
+            <ContactList contacts={contacts} onDeleteContact={deleteContacts}/>
         </div>
     );
 }
--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,7 +1,7 @@
 import styles from './Filter.module.css';
 import PropTypes from 'prop-types';
 
-export default function Filter({value, onChangeFilter})  {
+export default function Filter({filter, onChangeFilter})  {
     return (
         <label className={styles.filter_label}>
             Filter names
@@ -10,15 +10,15 @@ export default function Filter({value, onChangeFilter})  {
                 name="name"
                 pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Enter name"
-                value={value}
+                value={filter}
                 className={styles.filter_input}
-                onChange={onChangeFilter}
+                onChange={e => onChangeFilter(e.target.value)}
             />
         </label>
     );
 };
 
 Filter.propTypes = {
-    value: PropTypes.string.isRequired,
+    filter: PropTypes.string.isRequired,
     onChangeFilter: PropTypes.func.isRequired,
 }
